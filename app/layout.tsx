fdb59300e2369cc6fd364cc77832b7376f34a2ae
Aarav Shukla <aarav.shukla105@gmail.com>
2025-08-11 05:20:54 +0530

    Updated the title logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DevTyper",
   description: "Made by Aarav😎",
+  icons:{
+    icon:'/logo.png',
+  }
 };
 
 export default function RootLayout({
